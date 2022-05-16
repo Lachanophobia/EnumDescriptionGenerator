5b49c73356de3719f3c3209a1183ac109a7340a9
Thomas Kassos <thomaskassos@hotmail.com>
2022-05-16 23:46:42 +0100

    init commit

---
+++ src/Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using EnumExample;
+
+namespace Benchmark;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(AnotherTestEnum.ValueOne.GetDescription());
+        Console.WriteLine(AnotherTestEnum.ValueTwo.GetDescription());
+    }
+}
